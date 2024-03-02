f5900f77ccd420a6c449f4e05d2db6e74d9f3edd
victoria diaz <cvdiaz3@espe.edu.ec>
2024-03-01 19:15:34 -0500

    modelo paciente

--- Proyecto_CodeXplorers/src/proyecto/modelo/espe/Paciente.java
+++ Proyecto_CodeXplorers/src/proyecto/modelo/espe/Paciente.java
@@ -3,23 +3,80 @@ package proyecto.modelo.espe;
 
 
 public class Paciente {
-    private String Nombre;
+    private String cedula,nombre,apellido,genero,telefono,dia,horario;
     private String Contraseña;
+    private int fechaNacimiento;
+   
 
     public Paciente() {
     }
 
-    public Paciente(String Nombre, String Contraseña) {
-        this.Nombre = Nombre;
+    public Paciente(String cedula, String nombre, String apellido, String genero, String telefono, String dia, String horario, String Contraseña, int fechaNacimiento) {
+        this.cedula = cedula;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.genero = genero;
+        this.telefono = telefono;
+        this.dia = dia;
+        this.horario = horario;
         this.Contraseña = Contraseña;
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+    public String getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(String cedula) {
+        this.cedula = cedula;
     }
 
     public String getNombre() {
-        return Nombre;
+        return nombre;
     }
 
-    public void setNombre(String Nombre) {
-        this.Nombre = Nombre;
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getDia() {
+        return dia;
+    }
+
+    public void setDia(String dia) {
+        this.dia = dia;
+    }
+
+    public String getHorario() {
+        return horario;
+    }
+
+    public void setHorario(String horario) {
+        this.horario = horario;
     }
 
     public String getContraseña() {
@@ -29,5 +86,20 @@ public class Paciente {
     public void setContraseña(String Contraseña) {
         this.Contraseña = Contraseña;
     }
+
+    public int getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public void setFechaNacimiento(int fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+    @Override
+    public String toString() {
+        return "Paciente{" + "cedula=" + cedula + ", nombre=" + nombre + ", apellido=" + apellido + ", genero=" + genero + ", telefono=" + telefono + ", dia=" + dia + ", horario=" + horario + ", Contrase\u00f1a=" + Contraseña + ", fechaNacimiento=" + fechaNacimiento + '}';
+    }
     
-}
+    
+    
+    }
